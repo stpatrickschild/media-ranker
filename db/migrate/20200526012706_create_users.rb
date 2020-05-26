af6eb0d8be471184d741a8d06183f35bdf9f7bd4
Hannah Tekié <hannahtekie@gmail.com>
2020-05-25 18:48:57 -0700

    added users resources

--- db/migrate/20200526012706_create_users.rb
+++ db/migrate/20200526012706_create_users.rb
@@ -1,7 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-
+      t.string :name
+      t.datetime :joined_date
       t.timestamps
     end
   end
